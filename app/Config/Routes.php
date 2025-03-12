c31f6c995b2ef72ee3c165d9de59fba3b6d842dd
Sneha Goswami <snehagoswami431@gmail.com>
2025-03-12 13:45:07 +0530

    12_03_25

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -9,3 +9,5 @@ $routes->get('/', 'Home::index');
 $routes->get('/(:num)', 'Home::index/$1');
 $routes->get('delete/(:num)', 'Home::delete/$1');
 $routes->get('status/(:num)/(:any)', 'Home::status/$1/$2');
+$routes->match(["get", "post"], "create", 'Home::create');
+$routes->match(["get", "post"], "create/(:num)", 'Home::create/$1');
--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -5,11 +5,15 @@ namespace App\Controllers;
 use App\Models\CommonModel;
 use CodeIgniter\API\ResponseTrait;
 
+date_default_timezone_set("Asia/Kolkata");
 
 
 class Home extends BaseController
 {
-    use ResponseTrait;
+    use ResponseTrait; // CodeIgniter's ResponseTrait provides helper methods to 
+    //simplify sending API responses, such as JSON responses.
+    //It provides methods like respond(), respondCreated(), respondDeleted(), and respondError(),
+    // making it easy to send standard HTTP responses from your controller.
     private $model;
     function __construct()
     {
@@ -79,6 +83,58 @@ class Home extends BaseController
             $result = ["status" => 404, "data" => "No record found"];
         }
 
+        return $this->respond($result);
+    }
+    public function create($id = null)
+    {
+
+        if ($this->request->getMethod() == "POST") {
+            $name = $this->request->getVar("name");
+            $email = $this->request->getVar("email");
+            $title = $this->request->getVar("title");
+            $date = date('Y-m-d H:i:s');
+
+
+            if ($id == null) {
+                $data = [
+                    "name" => $name,
+                    "email" => $email,
+                    "title" => $title,
+                    "creation_date" => $date
+                ];
+                $insert = $this->model->insertValue("apis", $data);
+                if ($insert) {
+                    $result = ["status" => 201, "data" => "Data insert successfully."];
+                } else {
+                    $result = ["status" => 404, "data" => "Some Error Occurred."];
+                }
+            } else {
+                $selectdata = $this->model->selectRow("apis", ["id" => $id]);
+                if (!is_numeric($id) || $id <= 0) {
+                    return $this->respond(["status" => 400, "data" => "Invalid ID"]);
+                }
+
+                if (!empty($selectdata)) {
+                    $data = [
+                        "name" => $name,
+                        "email" => $email,
+                        "title" => $title,
+                        "updation_date" => $date
+
+                    ];
+                    $update = $this->model->updateValue("apis", ["id" => $id], $data);
+                    if ($update) {
+                        $result = ["status" => 201, "data" => "Data updated successfully."];
+                    } else {
+                        $result = ["status" => 404, "data" => "Some Error Occurred."];
+                    }
+                } else {
+                    $result = ["status" => 404, "data" => "No record found."];
+                }
+            }
+        } else {
+            $result = ["status" => 404, "data" => "No post method found."];
+        }
         return $this->respond($result);
     }
 }
